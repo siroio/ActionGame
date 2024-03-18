38b66819812b3d7dbcfccae6a3ba4b8d83f9f2c9
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-03-19 00:46:33 +0900

    add: enum

---
+++ Utility/Enum.h
@@ -0,0 +1,71 @@
+﻿#pragma once
+#include <concepts>
+
+template<class T>
+concept EnumClass = std::is_enum_v<T>;
+
+template<EnumClass T>
+class Enum
+{
+private:
+    using value_type = std::underlying_type_t<T>;
+    using enum_type = T;
+
+public:
+    Enum() = default;
+    Enum(value_type val) : value(static_cast<T>(val))
+    {}
+
+    value_type Value() const
+    {
+        return value;
+    }
+
+    explicit operator T() const
+    {
+        return value;
+    }
+
+    Enum& operator=(const value_type& val)
+    {
+        value = static_cast<T>(val);
+        return *this;
+    }
+
+    operator value_type() const
+    {
+        return static_cast<value_type>(value);
+    }
+
+    bool operator==(const Enum<enum_type>& other) const
+    {
+        return value == other.value;
+    }
+
+    bool operator==(const enum_type& other) const
+    {
+        return value == other;
+    }
+
+    bool operator==(const value_type& other) const
+    {
+        return value == static_cast<T>(other);
+    }
+
+    bool operator!=(const Enum<enum_type>& other) const
+    {
+        return value != other.value;
+    }
+
+    bool operator!=(const enum_type& other) const
+    {
+        return value != other;
+    }
+
+    bool operator!=(const value_type& other) const
+    {
+        return value != static_cast<T>(other);
+    }
+private:
+    T value;
+};
