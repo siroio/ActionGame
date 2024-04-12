a3c06f8383de25eb226694e84563034b249f8917
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-04-12 18:53:04 +0900

    アセットのロード処理を追加

--- App/App.cpp
+++ App/App.cpp
@@ -2,13 +2,30 @@
 #include "../Scene/TitleScene.h"
 #include "../Scene/PlayScene.h"
 #include "../Scene/ResultScene.h"
+#include "../Utility/AssetLoader.h"
 #include <GearsScene/SceneManager.h>
-
+#include <AudioManager.h>
 using namespace Glib;
+
+namespace
+{
+    constexpr char ANIMATION[]{ "Assets/List/Animation.csv" };
+    constexpr char AUDIO[]{ "Assets/List/Audio.csv" };
+    constexpr char EFFECT[]{ "Assets/List/Effect.csv" };
+    constexpr char MODEL[]{ "Assets/List/Model.csv" };
+    constexpr char SKYBOX[]{ "Assets/List/Skybox.csv" };
+}
 
 void App::Start()
 {
     ResiterScenes();
+    AssetLoader::Load(ANIMATION, AssetType::Animation);
+    AssetLoader::Load(AUDIO, AssetType::Audio);
+    AssetLoader::Load(EFFECT, AssetType::Effect);
+    AssetLoader::Load(MODEL, AssetType::Mesh);
+    AssetLoader::Load(SKYBOX, AssetType::Skybox);
+    AudioManager::AddSoundGroup(0);
+    AudioManager::SetSoundGroupVolume(0, 0.1f);
     SceneManager::LoadScene("PlayScene");
 }
 
