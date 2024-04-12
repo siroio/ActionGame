552cd43d881037ff484287bca7d386da9cafcd28
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-04-12 18:52:24 +0900

    入力を追加

---
+++ Input/Input.cpp
@@ -0,0 +1,30 @@
+﻿#include "Input.h"
+#include <GearsInput/InputSystem.h>
+#include <GearsInput/InputState.h>
+#include <GearsMath/Mathf.h>
+using namespace Glib;
+
+Vector2 Input::Move()
+{
+    return InputSystem::GetLeftStick();
+}
+
+Vector2 Input::CameraRotate()
+{
+    return InputSystem::GetRightStick();
+}
+
+bool Input::CameraLook()
+{
+    return InputSystem::GetButtonDown(GPADKey::RIGHT_TRIGGER);
+}
+
+bool Input::Attack()
+{
+    return InputSystem::GetButtonDown(GPADKey::B);
+}
+
+bool Input::Dodge()
+{
+    return InputSystem::GetButtonDown(GPADKey::A);
+}
