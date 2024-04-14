9b5deb0298a0695139e4229f232db29b3c735e93
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-04-15 07:57:39 +0900

    Damageableコンポーネントの追加

---
+++ Component/Common/Damageable.cpp
@@ -0,0 +1,65 @@
+﻿#include "Damageable.h"
+#include <GameObject.h>
+#include <Mathf.h>
+
+#include "../StateMachine/StateBehavior.h"
+
+Damageable::Damageable(int health, int maxHealth, int damageStateID, int deadStateID) :
+    health_{ health }, maxHealth_{ maxHealth }, damageStateID_{ damageStateID }, deadStateID_{ deadStateID }
+{}
+
+void Damageable::Start()
+{
+    if (stateBehavior_.expired())
+    {
+        stateBehavior_ = GameObject()->GetComponent<StateBehavior>();
+    }
+}
+
+int Damageable::Health() const
+{
+    return health_;
+}
+
+void Damageable::Health(int add)
+{
+    health_ = Mathf::Clamp(health_ + add, 0, maxHealth_);
+}
+
+float Damageable::HealthRate() const
+{
+    return health_ <= 0 ? 0.0f : health_ / maxHealth_;
+}
+
+bool Damageable::Invincible() const
+{
+    return invincible_;
+}
+
+void Damageable::Invincible(bool enable)
+{
+    invincible_ = enable;
+}
+
+bool Damageable::IsDead() const
+{
+    return health_ <= 0;
+}
+
+bool Damageable::TakeDamage(int power)
+{
+    if (invincible_) return false;
+
+    // ダメージを与える
+    health_ -= Mathf::Min(power, 0);
+
+    // 死亡していたらこれ以上ダメージを与えられないように攻撃を無効にする
+    if (IsDead()) Invincible(true);
+
+    // ステートマシンを持っている場合
+    // 死亡かダメージのステートへ推移
+    if (!stateBehavior_.expired()) return true;
+    stateBehavior_->ChangeState(IsDead() ? deadStateID_ : damageStateID_);
+
+    return true;
+}
---
+++ Component/Common/Damageable.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+#include <Component.h>
+#include <WeakPtr.h>
+
+class StateBehavior;
+
+class Damageable : public Component
+{
+public:
+    Damageable(int health, int maxHealth, int damageStateID, int deadStateID);
+
+    void Start();
+
+    /**
+     * @brief 残存体力
+     */
+    int Health() const;
+
+    /**
+     * @brief 体力の加算
+     */
+    void Health(int add);
+
+    /**
+     * @brief 体力の歩合を0~1で返す
+     */
+    float HealthRate() const;
+
+    /**
+     * @brief 攻撃が無効か？
+     */
+    bool Invincible() const;
+
+    /**
+     * @brief 攻撃を無効に設定
+     * @param enable
+     */
+    void Invincible(bool enable);
+
+    /**
+     * @brief 死亡しているか？
+     */
+    bool IsDead() const;
+
+    /**
+     * @brief ダメージを与える
+     * @param power 威力
+     * @return
+     */
+    bool TakeDamage(int power);
+
+private:
+    int health_{ 0 };
+    int maxHealth_{ 0 };
+    int damageStateID_{ -1 };
+    int deadStateID_{ -1 };
+    bool invincible_{ false };
+    Glib::WeakPtr<StateBehavior> stateBehavior_{ nullptr };
+};
