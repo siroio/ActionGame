2d40d1f1ce5a6f90ae63445d7d9f50e2dbbaa1f3
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-04-16 16:26:28 +0900

    タイマークラスを追加

---
+++ Component/Common/ElapsedTimer.cpp
@@ -0,0 +1,40 @@
+﻿#include "ElapsedTimer.h"
+#include <GameTimer.h>
+#include <GLGUI.h>
+#include <string>
+
+namespace
+{
+    const char* const MEASURE_TYPE[2]{ "Scaled", "Unscaled" };
+}
+
+ElapsedTimer::ElapsedTimer(const Mode& mode) :
+    mode_{ mode }
+{}
+
+void ElapsedTimer::Update()
+{
+    float deltaTime = mode_ == Mode::Scaled ?
+        Glib::GameTimer::DeltaTime() :
+        Glib::GameTimer::UnscaledDeltaTime();
+
+    elapsedTime_ += deltaTime;
+}
+
+void ElapsedTimer::Reset()
+{
+    elapsedTime_ = 0.0f;
+}
+
+float ElapsedTimer::Elapsed() const
+{
+    return elapsedTime_;
+}
+
+void ElapsedTimer::OnGUI()
+{
+    std::string label{ "Measure Type: " };
+    Glib::GLGUI::Text(label + MEASURE_TYPE[static_cast<unsigned int>(mode_)]);
+    std::string timeLabel{ "Elapsed Time: " };
+    Glib::GLGUI::Text(timeLabel + std::to_string(elapsedTime_));
+}
---
+++ Component/Common/ElapsedTimer.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <Component.h>
+
+/**
+ * @brief タイマークラス
+ * @brief コンポーネントがアクティブな時間を計測
+ */
+class ElapsedTimer : public Component
+{
+public:
+    enum class Mode
+    {
+        Scaled,
+        UnScaled,
+    };
+
+public:
+    ElapsedTimer(const Mode& mode = Mode::Scaled);
+    void Update();
+
+    /**
+     * @brief タイマーをリセット
+     */
+    void Reset();
+
+    /**
+     * @brief 経過時間の取得
+     * @return 秒
+     */
+    float Elapsed() const;
+
+private:
+    void OnGUI() override;
+
+private:
+    Mode mode_{ Mode::Scaled };
+    float elapsedTime_{ 0.0f };
+};
