11b47ce2a7d2ef55dbeeee8e120ac2c63005fa67
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-04-22 15:59:57 +0900

    AnimationInfoのコンストラクタを変更

--- Component/StateMachine/AnimationInfo.h
+++ Component/StateMachine/AnimationInfo.h
@@ -3,13 +3,13 @@
 struct AnimationInfo
 {
     AnimationInfo() = default;
-    AnimationInfo(int animationID, float blendTime = 0.1f, float startFrameOffset = 0.0f, bool loop = false) :
-        AnimationID{ animationID }, BlendTime{ blendTime },
-        StartFrameOffset{ startFrameOffset }, Loop{ loop }
+    AnimationInfo(int animationID, float frameOffset = 0.0f, float blendTime = 0.1f, bool loop = false) :
+        AnimationID{ animationID }, FrameOffset{ frameOffset },
+        BlendTime{ blendTime }, Loop{ loop }
     {}
 
     int AnimationID{ -1 };
     float BlendTime{ 0.0f };
-    float StartFrameOffset{ 0.0f };
+    float FrameOffset{ 0.0f };
     bool Loop{ false };
 };
