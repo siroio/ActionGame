67a08fe5da940ed390769ff179c857f02ba36aae
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-05-07 06:33:18 +0900

    プレイシーンの初期化を変更

--- Scene/PlayScene.cpp
+++ Scene/PlayScene.cpp
@@ -1,29 +1,33 @@
 ﻿#include "PlayScene.h"
+#include <Components/Canvas.h>
+#include <Components/MeshRenderer.h>
+#include <Components/MeshCollider.h>
 #include <GameObject.h>
 #include <GameObjectManager.h>
-#include <Components/Transform.h>
-#include <Components/MeshCollider.h>
-#include <Components/MeshRenderer.h>
-#include <Components/DirectionalLight.h>
 #include <SkyboxManager.h>
 #include <Physics.h>
 #include <Color.h>
 
+#include "../Character/Light/Light.h"
 #include "../Character/Camera/MainCamera.h"
 #include "../Character/Player/Player.h"
 #include "../Character/Enemy/Skeleton/Skeleton.h"
-#include "../Constant/GameObjectName.h"
+#include "../Character/Menu/SafeArea/SafeArea.h"
+#include "../Character/UI/HPGauge/HPGauge.h"
 #include "../Component/Camera/CameraController.h"
 #include "../Enum/CollisionLayer.h"
+#include <GameTimer.h>
+#include "../Character/Projectile/MagicArrow.h"
+
 
 using namespace Glib;
 
 namespace
 {
-    const Vector3 LIGHT_DIRECTION{ 30.0f, 40.0f, 0.0f };
+    const Vector3 LIGHT_DIRECTION{ 50.0f, -30.0f, 0.0f };
     const Color LIGHT_AMBIENT{ 0.7f, 0.7f, 0.7f, 1.0f };
     const Color LIGHT_DIFFUSE{ 0.7f, 0.7f, 0.7f, 1.0f };
-    const Color LIGHT_SPECULAR{ 0.5f, 0.5f, 0.5f, 0.5f };
+    const Color LIGHT_SPECULAR{ 0.1f, 0.1f, 0.1f, 0.1f };
     const unsigned int DEFAULT_SKYBOX{ 0 };
     const Vector3 MAIN_CAMERA_OFFSET{ 0.0f, 1.6f, 0.0f };
     const float MAIN_CAMERA_DISTANCE{ 2.5f };
@@ -31,15 +35,13 @@ namespace
 
 void PlayScene::Start()
 {
-    auto light = GameObjectManager::Instantiate(ObjectName::Light)->AddComponent<DirectionalLight>();
-    light->Ambient(LIGHT_AMBIENT);
-    light->Diffuse(LIGHT_DIFFUSE);
-    light->Specular(LIGHT_SPECULAR);
-    light->GameObject()->Transform()->EulerAngles(LIGHT_DIRECTION);
-
+    Light::Spawn(LIGHT_DIRECTION, LIGHT_AMBIENT, LIGHT_DIFFUSE, LIGHT_SPECULAR);
     SkyboxManager::SetSkybox(DEFAULT_SKYBOX);
     GameObjectPtr camera = MainCamera::Spawn(MAIN_CAMERA_OFFSET, MAIN_CAMERA_DISTANCE);
     GameObjectPtr player = Player::Spawn();
+    auto hpCanvas = GameObjectManager::Instantiate("HPCanvas");
+    hpCanvas->AddComponent<Canvas>();
+    HPGauge::Spawn(hpCanvas);
     Skeleton::Spawn(Vector3{ 0, 0, 5 }, Vector3::Zero(), Vector3::One());
 
     Physics::SetCollisionFlag(CollisionLayer::Player, CollisionLayer::PlayerAttack, false);
@@ -49,17 +51,23 @@ void PlayScene::Start()
     if (!player.expired())
     {
         auto controller = camera->GetComponentInParent<CameraController>();
-        controller->SetTarget(player->Transform()); // カメラにPlayerを注目するように設定
+        // カメラのターゲットにプレイヤーを設定
+        controller->SetTarget(player->Transform());
     }
 
+    MagicArrow::Spawn(Vector3{ 0.0f, 2.0f, 5.0f }, 0.0f, player);
+
+    // debug stage
     auto stage = GameObjectManager::Instantiate("Stage");
     stage->Layer(CollisionLayer::Stage);
     stage->Tag("Obstacle");
     auto mr = stage->AddComponent<MeshRenderer>();
-    mr->MeshID(2);
+    mr->MeshID(4);
     auto mc = stage->AddComponent<MeshCollider>();
-    mc->MeshID(2);
+    mc->MeshID(4);
     mc->FlipNormals(true);
+
+    SafeArea::Spawn();
 }
 
 void PlayScene::End()
