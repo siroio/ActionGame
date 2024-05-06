96d485651a75d3c059f34ec2eb08d57b2c48240e
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-05-07 06:32:09 +0900

    飛翔物ようコンポーネント追加

---
+++ Component/Projectile/Projectile.cpp
@@ -0,0 +1,74 @@
+﻿#include "Projectile.h"
+#include <Components/Transform.h>
+#include <Components/Rigidbody.h>
+#include <GameObject.h>
+#include <GameTimer.h>
+#include <GLGUI.h>
+
+using namespace Glib;
+
+Projectile::Projectile(const Glib::WeakPtr<Glib::Transform>& target) :
+    target_{ target }
+{}
+
+void Projectile::Start()
+{
+    rigidbody_ = GameObject()->GetComponent<Rigidbody>();
+}
+
+void Projectile::FixedUpdate()
+{
+    if (!target_.expired())
+    {
+        // ターゲットの方向へ向ける
+        Rotation();
+    }
+
+    if (!rigidbody_.expired())
+    {
+        // 移動させる
+        Move();
+    }
+}
+
+float Projectile::MoveSpeed() const
+{
+    return moveSpeed_;
+}
+
+void Projectile::MoveSpeed(float speed)
+{
+    moveSpeed_ = speed;
+}
+
+float Projectile::RotateSpeed() const
+{
+    return rotateSpeed_;
+}
+
+void Projectile::RotateSpeed(float speed)
+{
+    rotateSpeed_ = speed;
+}
+
+void Projectile::Rotation()
+{
+    const auto& transform = GameObject()->Transform();
+    Vector3 direction = (target_->Position() - transform->Position()).Normalized();
+    Quaternion rotation = Quaternion::LookRotation(direction);
+    float speed = rotateSpeed_ * GameTimer::FixedDeltaTime();
+    transform->Rotation(Quaternion::Slerp(transform->Rotation(), rotation, speed));
+}
+
+void Projectile::Move()
+{
+    Vector3 forward = GameObject()->Transform()->Forward();
+    Vector3 velocity = forward * moveSpeed_;
+    rigidbody_->LinearVelocity(velocity);
+}
+
+void Projectile::OnGUI()
+{
+    GLGUI::DragFloat("MoveSpeed", &moveSpeed_, 0.1f, 0.0f);
+    GLGUI::DragFloat("RotationSpeed", &rotateSpeed_, 0.1f, 0.0f);
+}
---
+++ Component/Projectile/Projectile.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <Component.h>
+
+namespace Glib
+{
+    class Transform;
+    class Rigidbody;
+}
+
+class Projectile : public Component
+{
+public:
+    Projectile(const Glib::WeakPtr<Glib::Transform>& target);
+    void Start();
+    void FixedUpdate();
+
+    float MoveSpeed() const;
+    void MoveSpeed(float speed);
+    float RotateSpeed() const;
+    void RotateSpeed(float speed);
+
+private:
+    void Rotation();
+    void Move();
+    void OnGUI() override;
+
+private:
+    Glib::WeakPtr<Glib::Rigidbody> rigidbody_{};
+    Glib::WeakPtr<Glib::Transform> target_{};
+    float moveSpeed_{ 10.0f };
+    float rotateSpeed_{ 5.0f };
+};
