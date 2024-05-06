948375115edf73e2aa12f6fae65cf99c84e5b161
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-05-07 06:31:21 +0900

    UIのセーフエリア追加

---
+++ Character/Menu/SafeArea/SafeArea.cpp
@@ -0,0 +1,22 @@
+﻿#include "SafeArea.h"
+#include <GameObject.h>
+#include <GameObjectManager.h>
+#include <Components/Canvas.h>
+#include <Components/Image.h>
+
+#include "../../Enum/TextureID.h"
+
+using namespace Glib;
+
+void SafeArea::Spawn()
+{
+    auto canvas = GameObjectManager::Instantiate("SafeAreaCanvas");
+    canvas->Active(false);
+    canvas->AddComponent<Canvas>();
+
+    auto safeArea = GameObjectManager::Instantiate("SafeArea");
+    safeArea->Transform()->Parent(canvas->Transform());
+    auto image = safeArea->AddComponent<Image>();
+    image->TextureID(TextureID::SafeArea);
+    image->Center(Vector2::Zero());
+}
---
+++ Character/Menu/SafeArea/SafeArea.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+
+struct SafeArea
+{
+    static void Spawn();
+};
