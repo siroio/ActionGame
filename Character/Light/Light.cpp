56b8450e04c04a0730faa417e704fc96d57915ef
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-05-07 06:33:27 +0900

    リファクタリング

---
+++ Character/Light/Light.cpp
@@ -0,0 +1,21 @@
+﻿#include "Light.h"
+#include <GameObject.h>
+#include <GameObjectManager.h>
+#include <Components/DirectionalLight.h>
+#include <Vector3.h>
+#include <Color.h>
+
+#include "../../Constant/GameObjectName.h"
+
+using namespace Glib;
+
+GameObjectPtr Light::Spawn(const Vector3& euler, const Color& ambient, const Color& diffuse, const Color& specular)
+{
+    auto directionalLight = GameObjectManager::Instantiate(ObjectName::Light);
+    auto light = directionalLight->AddComponent<DirectionalLight>();
+    light->Ambient(ambient);
+    light->Diffuse(diffuse);
+    light->Specular(specular);
+    light->GameObject()->Transform()->EulerAngles(euler);
+    return directionalLight;
+}
---
+++ Character/Light/Light.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include <GameObjectPtr.h>
+
+struct Vector3;
+struct Color;
+
+struct Light
+{
+    static GameObjectPtr Spawn(const Vector3& euler, const Color& ambient, const Color& diffuse, const Color& specular);
+};
