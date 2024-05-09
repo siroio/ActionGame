b2e6ced2f67651836b8a62903360b57556e34d88
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-05-10 08:32:03 +0900

    BGMコントローラー追加

---
+++ Component/Audio/BGMController.cpp
@@ -0,0 +1,33 @@
+﻿#include "BGMController.h"
+#include <Components/AudioSource.h>
+#include <GameObject.h>
+
+#include "../Fade/AudioFader.h"
+
+using namespace Glib;
+
+namespace
+{
+    constexpr float BGM_FADE_DURATION{ 0.5f };
+}
+
+void BGMController::Start()
+{
+    audioSource_ = GameObject()->GetComponent<AudioSource>();
+    fader_ = GameObject()->GetComponent<AudioFader>();
+}
+
+void BGMController::Update()
+{
+
+}
+
+void BGMController::Change(unsigned int id)
+{
+    if (nextBGMID == id || id < 0) return;
+    nextBGMID = id;
+    audioSource_->Volume(0.0f);
+    audioSource_->AudioID(id);
+    fader_->SetVolume(1.0f);
+    fader_->StartFade(BGM_FADE_DURATION);
+}
---
+++ Component/Audio/BGMController.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <Component.h>
+
+namespace Glib
+{
+    class AudioSource;
+}
+class AudioFader;
+
+class BGMController : public Component
+{
+public:
+    void Start();
+    void Update();
+    void Change(unsigned int id);
+
+private:
+    Glib::WeakPtr<Glib::AudioSource> audioSource_{};
+    Glib::WeakPtr<AudioFader> fader_{};
+    int nextBGMID{ -1 };
+};
