51eb1efc4df33ec7e2319407d7b0f50a5ab76663
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-05-14 02:52:42 +0900

    エフェクト生成コンポーネントの作成

---
+++ Component/Common/EffectSpawner.cpp
@@ -0,0 +1,22 @@
+﻿#include "EffectSpawner.h"
+#include <GameObject.h>
+#include <EventMsg.h>
+#include <Vector3.h>
+
+EffectSpawner::EffectSpawner(const SpawnFunction& function, unsigned int id) :
+    function_{ function }, messageID_{ id }
+{}
+
+void EffectSpawner::Spawn(const Vector3& position)
+{
+    if (function_ == nullptr) return;
+    const auto& transform = GameObject()->Transform();
+    Vector3 spawnPoint = position + transform->Rotation() * transform->Position();
+    function_(spawnPoint);
+}
+
+void EffectSpawner::ReceiveMsg(const Glib::EventMsg& msg)
+{
+    if (msg.MsgID() != messageID_) return;
+    Spawn(msg.Msg<Vector3>());
+}
---
+++ Component/Common/EffectSpawner.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <Component.h>
+#include <functional>
+
+namespace Glib
+{
+    class EffectSystem;
+    class EventMsg;
+}
+
+struct Vector3;
+
+class EffectSpawner : public Component
+{
+public:
+    using SpawnFunction = std::function<void(const Vector3&)>;
+
+public:
+    EffectSpawner(const SpawnFunction& function, unsigned int id);
+
+    void Spawn(const Vector3& position);
+    void ReceiveMsg(const Glib::EventMsg& msg);
+
+private:
+    SpawnFunction function_{};
+    int messageID_{ -1 };
+};
