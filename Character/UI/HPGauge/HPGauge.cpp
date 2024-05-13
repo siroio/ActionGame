ebf0160b0773f8d1b9982694200f6728170a0c09
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-05-14 02:53:07 +0900

    HPゲージの修正

--- Character/UI/HPGauge/HPGauge.cpp
+++ Character/UI/HPGauge/HPGauge.cpp
@@ -15,15 +15,18 @@ using namespace Glib;
 namespace
 {
     const Vector3 GAUGE_POSITION{ 120.0f, 70.0f, 0.0f };
-    const Color GAUGE_COLOR{ 0.0f, 1.0f, 0.0f };
-    const Color GAUGE_BG_COLOR{ 0.2f, 0.2f, 0.2f };
-    const Color GAUGE_FRAME_COLOR{ 0.1f, 0.1f, 0.1f };
+    const Color GAUGE_COLOR{ 0.0f, 1.0f, 0.0f, 1.0f };
+    const Color GAUGE_DAMAGED_COLOR{ 1.0f, 0.3f, 0.0f, 0.7f };
+    const Color GAUGE_BG_COLOR{ 0.2f, 0.2f, 0.2f, 0.6f };
+    const Color GAUGE_FRAME_COLOR{ 0.1f, 0.1f, 0.1f, 1.0f };
     constexpr float GAUGE_SPEED{ 20.0f };
+    constexpr float GAUGE_DAMAGED_SPEED{ 3.0f };
 }
 
 void HPGauge::Spawn(const GameObjectPtr& canvas)
 {
     auto hpBG = GameObjectManager::Instantiate("HPGaugeBG");
+    auto hpDamagedGauge = GameObjectManager::Instantiate("HPDamagedGauge");
     auto hpGauge = GameObjectManager::Instantiate("HPGauge");
     auto hpFrame = GameObjectManager::Instantiate("HPGaugeFrame");
 
@@ -37,13 +40,21 @@ void HPGauge::Spawn(const GameObjectPtr& canvas)
     auto player = GameObjectManager::Find(ObjectName::Player);
     auto damageable = player->GetComponent<Damageable>();
 
+    hpDamagedGauge->Transform()->Parent(hpBG->Transform());
+    auto hpDamagedImage = hpDamagedGauge->AddComponent<Image>();
+    auto hpDamagedController = hpDamagedGauge->AddComponent<GaugeController>(damageable);
+    hpDamagedController->Speed(GAUGE_DAMAGED_SPEED);
+    hpDamagedImage->TextureID(TextureID::HP_Gauge);
+    hpDamagedImage->Center(Vector2::Zero());
+    hpDamagedImage->Color(GAUGE_DAMAGED_COLOR);
+
     hpGauge->Transform()->Parent(hpBG->Transform());
-    auto hpImageGauge = hpGauge->AddComponent<Image>();
-    auto controller = hpGauge->AddComponent<GaugeController>(damageable);
-    controller->Speed(GAUGE_SPEED);
-    hpImageGauge->TextureID(TextureID::HP_Gauge);
-    hpImageGauge->Center(Vector2::Zero());
-    hpImageGauge->Color(GAUGE_COLOR);
+    auto hpGaugeImage = hpGauge->AddComponent<Image>();
+    auto hpController = hpGauge->AddComponent<GaugeController>(damageable);
+    hpController->Speed(GAUGE_SPEED);
+    hpGaugeImage->TextureID(TextureID::HP_Gauge);
+    hpGaugeImage->Center(Vector2::Zero());
+    hpGaugeImage->Color(GAUGE_COLOR);
 
     hpFrame->Transform()->Parent(hpBG->Transform());
     auto hpImageFrame = hpFrame->AddComponent<Image>();
