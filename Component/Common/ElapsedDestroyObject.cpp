0a66cbfe05c9ecf8f83131014dd606e5562a1df4
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-05-17 06:05:09 +0900

    時間経過でオブジェクトを削除するコンポーネントの追加

---
+++ Component/Common/ElapsedDestroyObject.cpp
@@ -0,0 +1,21 @@
+﻿#include "ElapsedDestroyObject.h"
+#include <GameObject.h>
+#include "ElapsedTimer.h"
+
+ElapsedDestroyObject::ElapsedDestroyObject(float time) :
+    destroyTime_{ time }
+{}
+
+void ElapsedDestroyObject::Start()
+{
+    timer_ = GameObject()->GetComponent<ElapsedTimer>();
+}
+
+void ElapsedDestroyObject::Update()
+{
+    if (timer_.expired()) return;
+    if (timer_->Elapsed() >= destroyTime_)
+    {
+        GameObject()->Destroy();
+    }
+}
---
+++ Component/Common/ElapsedDestroyObject.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <Component.h>
+
+class ElapsedTimer;
+
+class ElapsedDestroyObject : public Component
+{
+public:
+    ElapsedDestroyObject(float time);
+    void Start();
+    void Update();
+
+private:
+    Glib::WeakPtr<ElapsedTimer> timer_{};
+    float destroyTime_{ 0.0f };
+};
