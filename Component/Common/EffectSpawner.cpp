4bc9aa2c8fc7a404699d6359b086ec8f2e45993f
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-05-17 06:04:45 +0900

    位置のバグを修正

--- Component/Common/EffectSpawner.cpp
+++ Component/Common/EffectSpawner.cpp
@@ -11,12 +11,12 @@ void EffectSpawner::Spawn(const Vector3& position)
 {
     if (function_ == nullptr) return;
     const auto& transform = GameObject()->Transform();
-    Vector3 spawnPoint = position + transform->Rotation() * transform->Position();
+    Vector3 spawnPoint = position + transform->Position();
     function_(spawnPoint);
 }
 
 void EffectSpawner::ReceiveMsg(const Glib::EventMsg& msg)
 {
     if (msg.MsgID() != messageID_) return;
-    Spawn(msg.Msg<Vector3>());
+    Spawn(Vector3::Zero());
 }
