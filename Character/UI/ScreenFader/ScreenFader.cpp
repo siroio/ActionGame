460d6171f292953aed04e66191bfcbeb22a8b152
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-05-17 06:01:55 +0900

    画面のフェードを追加

---
+++ Character/UI/ScreenFader/ScreenFader.cpp
@@ -0,0 +1,30 @@
+﻿#include "ScreenFader.h"
+#include <GameObject.h>
+#include <GameObjectManager.h>
+#include <Components/Canvas.h>
+#include <Components/Image.h>
+#include "../../Component/Fade/UIFader.h"
+#include "../../Component/Common/ElapsedTimer.h"
+#include "../../Enum/TextureID.h"
+
+using namespace Glib;
+
+WeakPtr<UIFader> ScreenFader::Create(float fadeTime, bool onStartFade, TimerScale mode)
+{
+    auto gameObject = GameObjectManager::Instantiate("ScreenFader");
+    auto canvs = gameObject->AddComponent<Canvas>();
+    canvs->Order(100);
+
+    auto image = gameObject->AddComponent<Image>();
+    image->TextureID(TextureID::Fade);
+    image->Center(Vector2::Zero());
+
+    auto fader = gameObject->AddComponent<UIFader>();
+    fader->SetEase(Ease::OutQuart);
+    fader->SetEndAlpha(0.0f);
+    if (onStartFade) fader->StartFade(fadeTime);
+
+    auto timer = gameObject->AddComponent<ElapsedTimer>(mode);
+
+    return fader;
+}
---
+++ Character/UI/ScreenFader/ScreenFader.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+namespace Glib
+{
+    template<class T>
+    class WeakPtr;
+}
+
+class UIFader;
+enum class TimerScale;
+
+struct ScreenFader
+{
+    static Glib::WeakPtr<UIFader> Create(float fadeTime, bool onStartFade, TimerScale mode);
+};
